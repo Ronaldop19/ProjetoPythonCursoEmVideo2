0963bcd42d5068fb63edbbf68e7ba1eb4b73f282
Ronaldo Amorim <ronaldop.amorim@hotmail.com>
2022-09-28 01:31:53 -0300

    ex038
    Programa para pegar dois números inteiros e compará-los, dizendo qual é o maior ou se ambos são iguais.

---
+++ CursoemVideo/ex038.py
@@ -0,0 +1,9 @@
+n1 = int(input('Digite o primeiro número: '))
+n2 = int(input('Agora digite o segundo número: '))
+
+if n1 > n2:
+    print(f'O número primeiro valor é maior.')
+elif n2 > n1:
+    print(f'O número segundop valor  é maior.')
+else:
+    print('Os números são iguais.')
