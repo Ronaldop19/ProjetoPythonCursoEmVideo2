b082e8f14de8533289034000062e333ec91c73ff
Ronaldo Amorim <ronaldop.amorim@hotmail.com>
2022-12-05 22:31:58 -0300

    Delete ex060.py

--- CursoemVideo/ex060.py
+++
@@ -1,9 +0,0 @@
-numero = int(input('Digite um número qualquer para visualizar seu fatorial: '))
-cont = numero
-fator = 1
-while cont > 0:
-    print(f'{cont} ', end='')
-    print(' x ' if cont > 1 else ' = ', end='')
-    fator *= cont
-    cont -= 1
-print(f'{fator}', end='')
